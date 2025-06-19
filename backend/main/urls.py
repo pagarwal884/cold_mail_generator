af46f7c304298af6aedef13a43bcd00f555bf3ce
pagarwal884 <pagarwal1145@gmail.com>
2025-06-20 01:53:49 +0530

    Delete backend directory

--- backend/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.index_view,name='index_view'),
-    path('home/',views.home_view,name='home_view'),
-    path('upload-resume/', views.upload_resume, name='upload_resume'),
-]
